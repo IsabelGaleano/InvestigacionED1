142cc92974f55c217f28b7d8a014f78a47dddbb5
Daniel <dzunigar@ucenfotec.ac.cr>
2021-07-05 21:12:30 -0600

    Creacion del proyecto

---
+++ Bicola/Bicola/Bicola.cpp
@@ -0,0 +1,11 @@
+// Bicola.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+//Daniel
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
